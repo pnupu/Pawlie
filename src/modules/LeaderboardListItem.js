e7fcd88e0f7a9cb50bdc56fd866a162b5edc723c
rokas-sintra <rokas@sintra.ai>
2023-11-12 01:44:05 +0200

    finsl\

---
+++ src/modules/LeaderboardListItem.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const LeaderboardListItem = ({ name, points, highscore, position, src }) => {
+  // Define styles for different positions
+  const styles = {
+    gold: {
+      borderColor: "#E6B600",
+      backgroundColor: "#FFF8DF",
+      color: "#C09800",
+    },
+    silver: {
+      borderColor: "#CCCCCC",
+      backgroundColor: "#F4F4F4",
+      color: "#707070",
+    },
+    bronze: {
+      borderColor: "#D6743E",
+      backgroundColor: "#F6EDE6",
+      color: "#D6743E",
+    },
+    other: {
+      borderColor: "#CCCCCC",
+      backgroundColor: "#FFFFFF",
+      color: "#707070",
+    },
+  };
+
+  // Determine the style based on the position
+  let style;
+  switch (position) {
+    case 1:
+      style = styles.gold;
+      break;
+    case 2:
+      style = styles.silver;
+      break;
+    case 3:
+      style = styles.bronze;
+      break;
+    default:
+      style = styles.other;
+      break;
+  }
+
+  return (
+    <div className="flex gap-4 items-center">
+      <span className="text-dark">{position}</span>
+      <div
+        className={`flex flex-grow border-2 rounded-lg overflow-hidden items-center`}
+        style={{
+          borderColor: style.borderColor,
+          backgroundColor: style.backgroundColor,
+        }}
+      >
+        <img src={src} className="aspect-square object-cover w-12" />
+        <div className="grid grid-cols-5 gap-2 flex-grow px-3 py-2">
+          <span
+            className="font-bold uppercase col-span-3 truncate"
+            style={{ color: style.color }}
+          >
+            {name}
+          </span>
+          <span className="font-medium" style={{ color: style.color }}>
+            {points}
+          </span>
+          <span className="font-medium" style={{ color: style.color }}>
+            {highscore}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderboardListItem;
--- src/modules/leaderboardscreen.js
+++ src/modules/leaderboardscreen.js
@@ -1,12 +1,45 @@
+import LeaderboardListItem from "./LeaderboardListItem";
+
 function LeaderboardScreen() {
   return (
-    <div className="flex-grow flex flex-col overflow-y-auto p-5 md:p-6">
+    <div className="flex-grow flex flex-col overflow-y-auto p-5 md:p-6 ">
       <h1 className="text-2xl font-bold uppercase mb-8">Leaderboard</h1>
-      <p className="text-dark font-medium text-lg">Junction Hackathon 2023</p>
-      <p className="text-[#848484] font-medium">Time left: 1 day</p>
+      <p className="text-dark font-medium text-xl">Junction Hackathon 2023</p>
+      <p className="text-[#848484] text-xl font-medium">Time left: 1 day</p>
       <div id="content" className="flex flex-col flex-grow ">
-        <div id="list">
-          <div className="flex gap-4 items-center"></div>
+        <div className="flex justify-end mb-2 gap-4 mr-7">
+          <span className="font-medium text-sm  text-[#707070]">Score</span>
+          <span className="font-medium text-sm text-[#707070]">Stars</span>
+        </div>
+        <div id="list" className="flex flex-col gap-2">
+          <LeaderboardListItem
+            name={"Rokas"}
+            points={"10"}
+            highscore={"50"}
+            position={1}
+            src={"/hero-cover-min.jpg"}
+          ></LeaderboardListItem>
+          <LeaderboardListItem
+            name={"Rokas"}
+            points={"10"}
+            highscore={"50"}
+            position={2}
+            src={"/hero-cover-min.jpg"}
+          ></LeaderboardListItem>
+          <LeaderboardListItem
+            name={"Rokas"}
+            points={"10"}
+            highscore={"50"}
+            position={3}
+            src={"/hero-cover-min.jpg"}
+          ></LeaderboardListItem>
+          <LeaderboardListItem
+            name={"Rokas"}
+            points={"10"}
+            highscore={"50"}
+            position={4}
+            src={"/hero-cover-min.jpg"}
+          ></LeaderboardListItem>
         </div>
       </div>
     </div>
