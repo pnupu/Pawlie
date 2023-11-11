0bfae23f2e13c821c29d7b0e2e5508324ec7d605
mkrupskis <martynaskrupskis@gmail.com>
2023-11-12 01:04:00 +0200

    Add competitive mode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import GamePage from './components/GamePage'; // Your game page component
+import GameCompPage from './components/GameCompPage'; // Your game page component
 import PetPage from './components/PetPage'; // Your pet page component
 import QuizPage from './components/QuizPage'; // Your quiz page component
 import QRChallenge from './components/QRChallenge'; // Your QR challenge component
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/dev" element={<HomePage />} />
           <Route path="/game" element={<GamePage />} />
+          <Route path="/game-comp" element={<GameCompPage />} />
           <Route path="/pet" element={<PetPage />} />
           <Route path="/" element={<QuizPage />} />
           <Route path="/qr-game" element={<QRChallenge/>} />
